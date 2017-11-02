0c89bf87772db0139c1393df698a9b24e4e24c58
David Hale and Sarah Lindmar <sarah@>
2017-11-02 09:13:21 -0700

    basic files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MinesweeperComponent } from './minesweeper/minesweeper.component';
+import { RowComponent } from './row/row.component';
+import { TileComponent } from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MinesweeperComponent,
+    RowComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule,
